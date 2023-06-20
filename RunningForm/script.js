bec797cefe74290e603364c9b1f4a3983a2d40a1
Bruno Sedan <brunofyb@hotmail.com>
2023-06-20 22:22:30 +0200

    Running Form is done...next

--- RunningForm/script.js
+++ RunningForm/script.js
@@ -1,3 +1,4 @@
+/*  Variables */
 const distanceInput = document.getElementById("distance");
 const hourInput = document.getElementById("hour");
 const minuteInput = document.getElementById("minute");
@@ -6,7 +7,10 @@ const submitBtn = document.getElementById("Calcul");
 const speedSpan = document.getElementById("speed");
 const paceSpan = document.getElementById("pace");
 const timeInputs = document.querySelectorAll(".time");
-/* fonction Bouton*/
+const customInput = document.getElementById("custom");
+let newDistance;
+
+/* fonction Bouton Submit*/
 
 submitBtn.addEventListener("click", (e) => {
   e.preventDefault();
@@ -15,24 +19,56 @@ submitBtn.addEventListener("click", (e) => {
     parseInt(minuteInput.value) +
     "." +
     secondInput.value;
-
-  let newDistance = distanceInput.value / 1000;
+  /**********  CUSTOM || DISTANCE **********/
+  if (distanceInput.style.display == "none") {
+    console.log("yes");
+    newDistance = customInput.value;
+  } else {
+    console.log("no");
+    newDistance = distanceInput.value / 1000;
+  }
+  /********** RESULTAT **********/
   let resultSpeed = (newDistance / (newChrono / 60)).toFixed(2);
   let resultPace = (newChrono / newDistance).toFixed(2);
-
-  speedSpan.textContent = resultSpeed + "km/h";
-  paceSpan.textContent = resultPace + "min/km";
-
+  if (newChrono == 0 || distanceInput.value == 0) {
+    speedSpan.textContent = "0 " + "km/h";
+    paceSpan.textContent = " 0" + "min/km";
+  } else {
+    speedSpan.textContent =
+      newDistance +
+      "km à " +
+      resultSpeed +
+      "km/h en " +
+      parseInt(newChrono) +
+      "min";
+    paceSpan.textContent = resultPace + "min/km";
+  }
+  distanceInput.style.display = "";
+  customInput.style.display = "none";
+  distanceInput.value = 0;
   inputDefault();
 });
 
+/********** DEFAULT FONCTION**********/
 const inputDefault = () => {
   for (i = 0; i < timeInputs.length; i++)
     if (timeInputs[i].value > 59) {
       timeInputs[i].style.color = "red";
       speedSpan.textContent = "Veuillez Renseigner un chiffre de 0 à 59";
-      paceSpan.textContent = "-1";
+      paceSpan.textContent = "Et Voilou";
     } else {
       timeInputs[i].style.color = "black";
     }
 };
+
+/**********  CUSTOM FONCTION **********/
+const customDistance = () => {
+  distanceInput.addEventListener("click", (e) => {
+    if (distanceInput.value == "custom") {
+      distanceInput.style.display = "none";
+      customInput.style.display = "";
+    }
+  });
+};
+
+customDistance();
