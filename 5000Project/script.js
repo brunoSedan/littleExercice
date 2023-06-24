720da97af0c177ee1ee06e04d2edfb3340413f04
Bruno Sedan <brunofyb@hotmail.com>
2023-06-24 22:17:02 +0200

    code for 1 and 5 okay, brelan and suite off

--- 5000Project/script.js
+++ 5000Project/script.js
@@ -16,6 +16,8 @@ const allDice = document.querySelectorAll(".dice");
 
 /////////////////************---------Mooving Variable----------****************/////////////
 let nbChoice;
+let diceFive = 0;
+let rest = 0;
 let diceCount = 0;
 let nb = 0;
 playGame();
@@ -27,51 +29,58 @@ function playGame() {
   // resetDice();
   playBtn.addEventListener("click", (e) => {
     nb++;
+
     if (nb == 1) {
+      allDice.forEach((dice) => {
+        dice.classList.remove("display0");
+      });
       DiceRandom();
       handData.push(nbDice1, nbDice2, nbDice3, nbDice4, nbDice5);
-      dice1.textContent = nbDice1;
-      dice1.classList.add("dice" + nbDice1);
-      dice2.textContent = nbDice2;
-      dice2.classList.add("dice" + nbDice2);
-      dice3.textContent = nbDice3;
-      dice3.classList.add("dice" + nbDice3);
-      dice4.textContent = nbDice4;
-      dice4.classList.add("dice" + nbDice4);
-      dice5.textContent = nbDice5;
-      dice5.classList.add("dice" + nbDice5);
-      analyse();
+      diceDisplay();
+      searchBingo();
+      searchRest();
       replay();
     }
+    playBtn.style.display = "none";
+    // console.log(nb);
   });
 }
 
 //** ----Fonction Dice Display Reset----- */
-function resetDice() {
-  allDice.forEach((dice) => {
-    dice.textContent = "";
-    dice.classList.remove("selected", "bingo", "suite", "rest", "brelan");
-    // for (i = 0; i <= 5; i++) {
-    //   dice.classList.remove("dice" + i);
-    // }
-  });
-}
+// function resetDice() {
+//   allDice.forEach((dice) => {
+//     dice.textContent = "";
+//     dice.classList.remove("selected", "bingo", "suite", "rest", "brelan");
+//     // for (i = 0; i <= 5; i++) {
+//     //   dice.classList.remove("dice" + i);
+//     // }
+//   });
+// }
 //***------Second Lancer */
 function secondThrow() {
   secondBtn.addEventListener("click", (e) => {
+    nb++;
     if (nb == 1) {
-      nb++;
       throwResult.textContent = "";
       AnalyseCtn.textContent = "";
       allDice.forEach((dice) => {
-        if (!dice.classList.contains("selected")) {
-          dice.textContent = random(1, 6);
+        if (!dice.classList.contains("rest")) {
+          for (i = 0; i < 7; i++) {
+            dice.classList.remove("display" + i);
+          }
+          dice.value = random(1, 6);
+          dice.classList.add("display" + dice.value);
+        }
+        if (dice.classList.contains("rest")) {
+          dice.classList.remove("selected");
+          dice.classList.remove("dice");
         }
       });
-      analyse();
+      searchRest();
+      searchBingo();
+      replay();
     }
     // replay();
-    console.log(nb);
   });
 }
 
@@ -81,11 +90,20 @@ function replay() {
     if (dice.classList.contains("selected")) {
       diceCount++;
     }
+    if (dice.classList.contains("rest")) {
+      rest++;
+    }
   });
+  if (rest == 5) {
+    diceCount = 5;
+  }
+
+  console.log(rest);
   switch (diceCount) {
     case 0:
       nextCtn.textContent = "vous avez perdu";
       diceCount = 0;
+      rest = 0;
       break;
     case 5:
       nextCtn.textContent = "vous avez tout gagné";
@@ -95,89 +113,49 @@ function replay() {
     default:
       nextCtn.textContent = "vous pouvez rejouer des des";
       secondThrow();
+      nb--;
+      rest = 0;
       diceCount = 0;
       break;
-  }
-}
-/**--------- FONCTION ANALYSE ----------*/
-
-function analyse() {
-  if (searchBingo()) {
-    AnalyseCtn.textContent = `C'est un bingo!`;
-    allDice.forEach((dice) => {
-      dice.classList.add("bingo", "selected");
-    });
-  } else if (searchBgSuite()) {
-    AnalyseCtn.textContent = `Une Chance une grande suite!`;
-    allDice.forEach((dice) => {
-      dice.classList.add("suite", "selected");
-    });
-  } else if (searchLtlSuite()) {
-    AnalyseCtn.textContent = `Une Chance Petite suite!`;
-    allDice.forEach((dice) => {
-      dice.classList.add("suite", "selected");
-    });
-  } else if (searchBrelan()) {
-    newData = handData.filter((number) => number != nbChoice);
-    AnalyseCtn.textContent = `Un petit brelan de ${nbChoice}!`;
-    searchRest(newData);
-    allDice.forEach((dice) => {
-      if (dice.textContent == nbChoice) {
-        dice.classList.add("brelan", "selected"); //***SOuligner juste 3 nbCHoice *!!!!/
-      }
-    });
-  } else if ((newData = handData)) {
-    searchRest(newData);
   }
 }
 
 /**--------------FONCTION EACH POSIBILITIE---------------**/
 
-function searchLtlSuite() {
-  const DataLtlSuite = handData.sort();
-  const ltlSuite = [1, 2, 3, 4, 5];
-  if (ltlSuite.toString() == DataLtlSuite.toString()) {
-    return true;
-  }
-}
-
-function searchBgSuite() {
-  const DataBgSuite = handData.sort();
-  const BgSuite = [2, 3, 4, 5, 6];
-  if (BgSuite.toString() == DataBgSuite.toString()) {
-    return true;
-  }
-}
-
-function searchBrelan() {
-  for (i = 0; i <= handData.length; i++) {
-    const newData = handData.filter((el) => el === i);
-    if (newData.length >= 3 && !(newData.length == 5 && newData[1] == 5)) {
-      nbChoice = newData[1];
-      return true;
-    }
-  }
-}
-
 function searchBingo() {
-  const newData = handData.sort().filter((el) => el === 5);
-  if (newData.length == 5) {
-    return true;
-  }
+  allDice.forEach((dice) => {
+    if ((dice.value = 5)) {
+      diceFive++;
+    }
+    if (diceFive == 5) {
+      AnalyseCtn.textContent = "bingo!!!";
+    }
+  });
 }
 
-function searchRest(newData) {
-  fiveData = newData.filter((number) => number == 5);
-  oneData = newData.filter((number) => number == 1);
-  if (fiveData.length > 0 || oneData.length > 0) {
-    AnalyseCtn.textContent += `Vous avez des 5 ou des 1`;
-    allDice.forEach((dice) => {
-      if (dice.textContent == 1 || dice.textContent == 5) {
-        dice.classList.add("rest", "selected");
-      }
-    });
-  }
+function searchRest() {
+  allDice.forEach((dice) => {
+    if (
+      dice.classList.contains("dice") &&
+      (dice.value == 5 || dice.value == 1)
+    ) {
+      dice.classList.add("selected", "rest");
+    }
+  });
 }
+
+// function searchRest(newData) {
+//   fiveData = newData.filter((number) => number == 5);
+//   oneData = newData.filter((number) => number == 1);
+//   if (fiveData.length > 0 || oneData.length > 0) {
+//     AnalyseCtn.textContent += `Vous avez des 5 ou des 1`;
+//     allDice.forEach((dice) => {
+//       if (dice.value == 1 || dice.value == 5) {
+//         dice.classList.add("rest", "selected");
+//       }
+//     });
+//   }
+// }
 //*********Fonction DICE********* */
 /** Fonction Aleatoire */
 function random(min, max) {
@@ -187,17 +165,55 @@ function random(min, max) {
   //The maximum is inclusive and the minimum is inclusive
 }
 /*-----Math Random---------- */
-function DiceRandom() {
-  nbDice1 = random(1, 6);
-  nbDice2 = random(1, 6);
-  nbDice3 = random(1, 6);
-  nbDice4 = random(1, 6);
-  nbDice5 = random(1, 6);
-}
 // function DiceRandom() {
-//   nbDice1 = 5;
-//   nbDice2 = 5;
-//   nbDice3 = 5;
-//   nbDice4 = 5;
-//   nbDice5 = 5;
+//   nbDice1 = random(1, 6);
+//   nbDice2 = random(1, 6);
+//   nbDice3 = random(1, 6);
+//   nbDice4 = random(1, 6);
+//   nbDice5 = random(1, 6);
+// }
+function DiceRandom() {
+  nbDice1 = 5;
+  nbDice2 = 5;
+  nbDice3 = 5;
+  nbDice4 = 5;
+  nbDice5 = 5;
+}
+function diceDisplay() {
+  dice1.value = nbDice1;
+  dice1.classList.add("display" + nbDice1);
+  dice2.value = nbDice2;
+  dice2.classList.add("display" + nbDice2);
+  dice3.value = nbDice3;
+  dice3.classList.add("display" + nbDice3);
+  dice4.value = nbDice4;
+  dice4.classList.add("display" + nbDice4);
+  dice5.value = nbDice5;
+  dice5.classList.add("display" + nbDice5);
+}
+
+// function searchLtlSuite() {
+//   const DataLtlSuite = handData.sort();
+//   const ltlSuite = [1, 2, 3, 4, 5];
+//   if (ltlSuite.toString() == DataLtlSuite.toString()) {
+//     return true;
+//   }
+// }
+
+// function searchBgSuite() {
+//   const DataBgSuite = handData.sort();
+//   const BgSuite = [2, 3, 4, 5, 6];
+//   if (BgSuite.toString() == DataBgSuite.toString()) {
+//     return true;
+//   }
+// }
+
+// function searchBrelan() {
+//   for (i = 0; i <= handData.length; i++) {
+//     const newData = handData.filter((el) => el === i);
+//     if (newData.length >= 3 && !(newData.length == 5 && newData[1] == 5)) {
+//       nbChoice = newData[1];
+//       return true;
+//     }
+//   }
 // }
